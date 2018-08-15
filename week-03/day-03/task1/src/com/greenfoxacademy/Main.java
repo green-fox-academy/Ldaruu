33a9e78b20a2df8e21d6e1efccd3a03ef4dadb59
Laszlo Daru <daruu@hotmail.com>
2018-08-15 12:04:41 +0200

    first task of the day

--- week-03/day-03/task1/src/com/greenfoxacademy/Main.java
+++ week-03/day-03/task1/src/com/greenfoxacademy/Main.java
@@ -2,7 +2,23 @@ package com.greenfoxacademy;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+  public static void main(String[] args) {
+    PostIt one = new PostIt();
+    one.backroundcolor = "black";
+    one.textColor = "red";
+    one.text = "Idea1";
+
+    PostIt two = new PostIt();
+    two.backroundcolor="orange";
+    two.textColor="black";
+    two.text="Awesome";
+
+    PostIt three = new PostIt();
+    three.backroundcolor= "red";
+    three.textColor="blue";
+    three.text="Superb";
+
+    System.out.println( one.backroundcolor);
+    // write your code here
+  }
 }
---
+++ week-03/day-03/task1/src/com/greenfoxacademy/PostIt.java
@@ -0,0 +1,10 @@
+package com.greenfoxacademy;
+
+class PostIt {
+
+  String backroundcolor;
+  String text;
+  String textColor;
+}
+
+
