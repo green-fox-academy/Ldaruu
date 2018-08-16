de894359e72af685e4a9e5d4dd5a78a0b4c334ab
Laszlo Daru <daruu@hotmail.com>
2018-08-16 11:08:30 +0200

    NumbAdder

---
+++ week-03/day-04/src/com/greenfoxacademy/NumberAdder.java
@@ -0,0 +1,19 @@
+package com.greenfoxacademy;
+
+import java.util.Scanner;
+
+public class NumberAdder {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Plese give me a number!");
+    int numb = input.nextInt();
+    System.out.println(addNum(numb));
+  }
+  public static int addNum(int n){
+    if (n ==0){
+      return n;
+    }else {
+      return addNum(n-1)+n;
+    }
+  }
+}
