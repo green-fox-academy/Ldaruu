efd4e7c8592c7a53a113931ab6c452b4352e3a36
Laszlo Daru <daruu@hotmail.com>
2018-08-16 15:20:35 +0200

    some line changes is String2

--- week-03/day-04/src/com/greenfoxacademy/String2.java
+++ week-03/day-04/src/com/greenfoxacademy/String2.java
@@ -2,16 +2,16 @@ package com.greenfoxacademy;
 
 public class String2 {
   public static void main(String[] args) {
-    System.out.println(changeX("joijoixjoijxoixjixj"));
+    System.out.println(removeX("joijoixjoijxoixjixj"));
 
   }
-  public static String changeX(String word) {
+  public static String removeX(String word) {
     if (word.length() == 0) {
       return "";
     } else if (word.charAt(0) == 'x') {
-      return ""+ changeX(word.substring(1));
+      return ""+ removeX(word.substring(1));
     } else {
-      return word.charAt(0) + changeX(word.substring(1));
+      return word.charAt(0) + removeX(word.substring(1));
     }
 
   }
---
+++ week-03/day-04/src/com/greenfoxacademy/String3.java
@@ -0,0 +1,8 @@
+package com.greenfoxacademy;
+
+public class String3 {
+  public static void main(String[] args) {
+
+  }
+  
+}
