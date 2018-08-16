fc487db82f31a375f698640f54f876147693da31
Laszlo Daru <daruu@hotmail.com>
2018-08-16 15:11:46 +0200

    String2

--- week-03/day-04/src/com/greenfoxacademy/String2.java
+++ week-03/day-04/src/com/greenfoxacademy/String2.java
@@ -1,21 +1,18 @@
 package com.greenfoxacademy;
 
-public class String1 {
+public class String2 {
   public static void main(String[] args) {
-    System.out.println(changeX("xpcxxlkxyxux"));
+    System.out.println(changeX("joijoixjoijxoixjixj"));
 
   }
-
   public static String changeX(String word) {
     if (word.length() == 0) {
       return "";
     } else if (word.charAt(0) == 'x') {
-      return 'y'+ changeX(word.substring(1));
+      return ""+ changeX(word.substring(1));
     } else {
       return word.charAt(0) + changeX(word.substring(1));
     }
 
   }
 }
-// Given a string, compute recursively (no loops) a new string where all the
-// lowercase 'x' chars have been changed to 'y' chars.
