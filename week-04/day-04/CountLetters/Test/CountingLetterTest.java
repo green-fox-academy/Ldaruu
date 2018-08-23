8d114d3237c9597ac4fb30ca24533670ae30c36b
Laszlo Daru <daruu@hotmail.com>
2018-08-23 23:26:15 +0200

    Test created for Wordmaking method

--- week-04/day-04/CountLetters/Test/CountingLetterTest.java
+++ week-04/day-04/CountLetters/Test/CountingLetterTest.java
@@ -15,4 +15,14 @@ public class CountingLetterTest {
     myHas.put("b", 2);
     Assert.assertEquals(myHas, CountLetters.countLetters(myString));
   }
+
+  @Test
+  public void makingWordsTest(){
+    myString = "asdas";
+    HashMap myHas =new HashMap<String,Integer>();
+    myHas.put("a",2);
+    myHas.put("s",2);
+    myHas.put("d",1);
+    Assert.assertEquals("as",CountLetters.wordMaker(myHas,2));
+  }
 }
--- week-04/day-04/CountLetters/src/CountLetters.java
+++ week-04/day-04/CountLetters/src/CountLetters.java
@@ -3,6 +3,18 @@ import java.util.HashMap;
 public class CountLetters {
   public static void main(String[] args) {
     System.out.println(countLetters("apple"));
+    System.out.println(wordMaker(countLetters("iiidddslklk"), 3));
+  }
+
+  public static String wordMaker(HashMap<String, Integer> map, Integer num) {  //iterating through the HashMap and creating words from
+                                                                               // the letters which are num times in the Map
+    String myWord = "";
+    for (String key : map.keySet()) {
+      if (map.get(key).equals(num)) {
+        myWord += key;
+      }
+    }
+    return myWord;
   }
 
   public static HashMap<String, Integer> countLetters(String word) {
