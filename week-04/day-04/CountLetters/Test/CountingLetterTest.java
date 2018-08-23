345dad61c35d5d73d8cf977c828a2bbb4c90ed33
Laszlo Daru <daruu@hotmail.com>
2018-08-23 20:52:12 +0200

    Counting Letters Final

--- week-04/day-04/CountLetters/Test/CountingLetterTest.java
+++ week-04/day-04/CountLetters/Test/CountingLetterTest.java
@@ -1,4 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+
 public class CountingLetterTest {
 
+  String myString;
 
+  @Test
+  public void countLettersTest() {
+    myString = "baba";
+    HashMap myHas = new HashMap<Integer, String>();
+    myHas.put("a", 2);
+    myHas.put("b", 2);
+    Assert.assertEquals(myHas, CountLetters.countLetters(myString));
+  }
 }
--- week-04/day-04/CountLetters/src/CountLetters.java
+++ week-04/day-04/CountLetters/src/CountLetters.java
@@ -1,10 +1,13 @@
 import java.util.HashMap;
 
 public class CountLetters {
+  public static void main(String[] args) {
+    System.out.println(countLetters("apple"));
+  }
 
-  public HashMap<String, Integer> countLetters(String word) {
+  public static HashMap<String, Integer> countLetters(String word) {
     HashMap<String, Integer> dictionary = new HashMap<>();
-    for (int i = 0; i < dictionary.size(); i++) {
+    for (char i : word.toCharArray()) {
       String letter = i + "";
       if (!dictionary.containsKey(letter)) {
         dictionary.put(letter, 1);
