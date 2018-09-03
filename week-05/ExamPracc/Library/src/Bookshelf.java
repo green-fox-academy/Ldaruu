5d93e47a10a88e126fcfe48d26e3fa4c8e05cbae
Laszlo Daru <daruu@hotmail.com>
2018-09-03 21:54:26 +0200

    Library finalised however its printing too many things when bookshelf is empty

--- week-05/ExamPracc/Library/src/Bookshelf.java
+++ week-05/ExamPracc/Library/src/Bookshelf.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Bookshelf {
@@ -14,16 +15,83 @@ public class Bookshelf {
   public Bookshelf() {
   }
 
-  public void add (Book book) {
+  public void add(Book book) {
     bookShelf.add(book);
   }
 
   public void remove(String title) {
     for (int i = 0; i < bookShelf.size(); i++) {
-      if (bookShelf.get(i).equals(title)) {
+      if (bookShelf.get(i).title == title) {
         bookShelf.remove(bookShelf.get(i));
       }
     }
   }
 
+  public String favouriteAuth() {
+    HashMap<String, Integer> authors = new HashMap<>();
+    String favourite = "";
+    if (bookShelf.size() == 0) {
+      return "This bookshelf is empty!";
+    } else {
+      for (int i = 0; i < bookShelf.size(); i++) {
+        if (!authors.containsKey(bookShelf.get(i).author)) {
+          authors.put(bookShelf.get(i).author, 1);
+        } else {
+          authors.put(bookShelf.get(i).author, +1);
+        }
+      }
+      int bestauthorcounter = 0;
+      for (String bestAuthor : authors.keySet()) {
+        if (bestauthorcounter < authors.get(bestAuthor)) {
+          favourite = bestAuthor;
+          bestauthorcounter = authors.get(bestAuthor);
+        }
+      }
+    }
+    return favourite;
+  }
+
+  public String earliestRealise() {
+    if (bookShelf.size() == 0) {
+      return "Bookshelf is empty!";
+    } else {
+      int erliestRe = bookShelf.get(0).releaseYear;
+      for (int i = 0; i < bookShelf.size(); i++) {
+        if (bookShelf.get(i).releaseYear < erliestRe) {
+          erliestRe = bookShelf.get(i).releaseYear;
+        }
+      }
+      String earliestPublis = "Earliest released: ";
+      for (int i = 0; i < bookShelf.size(); i++) {
+        if (bookShelf.get(i).releaseYear == erliestRe) {
+          earliestPublis += bookShelf.get(i).toString();
+        }
+      }
+      return earliestPublis;
+    }
+  }
+
+  public String latestRealise() {
+    if (bookShelf.size() == 0) {
+      return "Bookshelf is empty";
+    } else {
+      int latestRe = bookShelf.get(0).releaseYear;
+      for (int i = 0; i < bookShelf.size(); i++) {
+        if (bookShelf.get(i).releaseYear > latestRe) {
+          latestRe = bookShelf.get(i).releaseYear;
+        }
+      }
+      String latestPublis = "Latest released; ";
+      for (int i = 0; i < bookShelf.size(); i++) {
+        if (bookShelf.get(i).releaseYear == latestRe) {
+          latestPublis += bookShelf.get(i).toString();
+        }
+      }
+      return latestPublis;
+    }
+  }
+
+  public String toString() {
+    return "You have: " + bookShelf.size() + " books\n" + earliestRealise() + "\n" + latestRealise() + "\nFavourite author: " + favouriteAuth();
+  }
 }
