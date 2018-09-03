f1168a04801327b88fb98457ad24dc77815e9c40
Laszlo Daru <daruu@hotmail.com>
2018-09-03 17:53:41 +0200

    EndsWithS done with regular expression

--- week-05/ExamPracc/EndsWithS/src/EndsWithS.java
+++ week-05/ExamPracc/EndsWithS/src/EndsWithS.java
@@ -1,3 +1,11 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EndsWithS {
   public static void main(String[] args) {
     /**
@@ -9,5 +17,34 @@ public class EndsWithS {
      * the correct output for the 'base.txt' is: 26
      * hint: Keep in mind that there may punctuation marks at the end of the words
      */
+    String filename = "doc/myfile.txt";
+    System.out.println(endsWithS(filename));
+  }
+
+  public static List<String> readAllLines(String filename) {
+    List<String> alllines = new ArrayList<>();
+    try {
+      Path path = Paths.get(filename);
+      alllines = Files.readAllLines(path);
+    } catch (Exception e) {
+      System.out.println("Uups something went wrong while reading the file" + e);
+    }
+    return alllines;
+  }
+
+  public static int endsWithS(String filename) {
+    List<String> allLines = readAllLines(filename);
+    Pattern pattern = Pattern.compile("\\b[A-Za-z]*s\\b"); // The \b matches the beginning and end of a word i.e. space,
+    // tab or newline, or the beginning or end of a string. The [A-Za-z] will match any letter, and the * means that 0+ get matched.
+    //Finally there is the s.
+    
+    int sum = 0;
+    for (String line : allLines) {
+      Matcher matcher = pattern.matcher(line);
+      while (matcher.find()) {
+        sum++;
+      }
+    }
+    return sum;
   }
 }
