53dcd34c0be27b841da5b220fa1d04a8c38651b7
Laszlo Daru <daruu@hotmail.com>
2018-09-03 14:36:55 +0200

    EndsWithS created and coppied the txt file

---
+++ week-05/ExamPracc/EndsWithS/src/EndsWithS.java
@@ -0,0 +1,13 @@
+public class EndsWithS {
+  public static void main(String[] args) {
+    /**
+     * Create a method called `endsWithS` that takes a filename as a string
+     * and returns the number of the words, that ends with 's'.
+     *
+     * Example cases:
+     *
+     * the correct output for the 'base.txt' is: 26
+     * hint: Keep in mind that there may punctuation marks at the end of the words
+     */
+  }
+}
