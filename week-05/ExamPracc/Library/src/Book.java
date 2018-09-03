2e6c8fc002fa436f8109489beb65b075192476e9
Laszlo Daru <daruu@hotmail.com>
2018-09-03 20:41:03 +0200

    Book class ready, Bookshelf in progress

--- week-05/ExamPracc/Library/src/Book.java
+++ week-05/ExamPracc/Library/src/Book.java
@@ -3,4 +3,29 @@ public class Book {
   // Create a constructor for setting those values
   // Book should have a toString() method that returns a string in this format:
   // Douglas Adams : The Hitchhiker's Guide to the Galaxy (1979)
+  String author;
+  String title;
+  int releaseYear;
+
+  public Book(String author, String title, int releaseYear) {
+    this.author = author;
+    this.title = title;
+    this.releaseYear = releaseYear;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public int getReleaseYear() {
+    return releaseYear;
+  }
+
+  public String toString() {
+    return getAuthor() + " : " + getTitle() + "\t(" + getReleaseYear() + ")";
+  }
 }
--- week-05/ExamPracc/Library/src/Bookshelf.java
+++ week-05/ExamPracc/Library/src/Bookshelf.java
@@ -1,9 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bookshelf {
   // Create a Bookshelf class that has a list of books in it
   // We should be able to add and remove books
   // We should be able to query the favourite author (who has written the most books in the shelf)
   // We should be able to query the earliest published books.
   // We should be able to query the latest published books.
-  // Bookshelf should have a toString() method which give us information about the number of books, the earliest and the latest released books, and the favourite author
+  // Bookshelf should have a toString() method which give us information about the number of books,
+  // the earliest and the latest released books, and the favourite author
+  List<Book> bookShelf = new ArrayList<>();
+
+  public Bookshelf() {
+  }
+
+  public void add (Book book) {
+    bookShelf.add(book);
+  }
+
+  public void remove(String title) {
+    for (int i = 0; i < bookShelf.size(); i++) {
+      if (bookShelf.get(i).equals(title)) {
+        bookShelf.remove(bookShelf.get(i));
+      }
+    }
+  }
 
 }
