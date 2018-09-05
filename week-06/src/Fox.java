f4bbde2700e818824b98ff7ceb964592cc3c4803
Laszlo Daru <daruu@hotmail.com>
2018-09-05 21:39:57 +0200

    Fox Class created

---
+++ week-06/src/Fox.java
@@ -0,0 +1,14 @@
+public class Fox {
+  //  Create a Fox class with 3 properties(name, type, color) Fill a list with at least 5 foxes,
+//  it's up to you how you name/create them! Write a Stream Expression to find the foxes with green color!
+//  Write a Stream Expression to find the foxes with green color and pallida type!
+  private String name;
+  private String type;
+  private String color;
+
+  public Fox(String name, String type, String color) {
+    this.name = name;
+    this.type = type;
+    this.color = color;
+  }
+}
