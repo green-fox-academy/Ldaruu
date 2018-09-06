a1de798e4c7964fc55ec69e76384ca29998f8ba1
Laszlo Daru <daruu@hotmail.com>
2018-09-06 19:00:35 +0200

    some line changes is Reservation

--- week-03/day-05/Reservation/src/Main.java
+++ week-03/day-05/Reservation/src/Main.java
@@ -3,16 +3,17 @@ import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
-    List<Reservation> bookingList= reservations(10);
-    for (Reservation current:bookingList) {
+    List<Reservation> bookingList = reservations(10);
+    for (Reservation current : bookingList) {
       current.printBooking();
-      
+
     }
 
   }
-  public static List<Reservation> reservations(int bookingnum){
-    List<Reservation>bookings =new ArrayList<>();
-    for (int i = 0; i <bookingnum ; i++) {
+
+  public static List<Reservation> reservations(int bookingNum) {
+    List<Reservation> bookings = new ArrayList<>();
+    for (int i = 0; i < bookingNum; i++) {
       bookings.add(new Reservation());
     }
     return bookings;
