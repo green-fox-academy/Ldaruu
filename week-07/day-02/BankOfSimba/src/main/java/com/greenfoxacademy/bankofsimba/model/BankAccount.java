7c328efa619bdfe8fa758b955d2d34a2f0c9d618
Laszlo Daru <daruu@hotmail.com>
2018-09-18 13:25:09 +0200

    BanckAccout Class created

---
+++ week-07/day-02/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/model/BankAccount.java
@@ -0,0 +1,40 @@
+package com.greenfoxacademy.bankofsimba.model;
+
+public class BankAccount {
+  private String name;
+  private long balance;
+  private  String animalType;
+
+  public BankAccount() {
+  }
+
+  public BankAccount(String name, long balance, String animalType) {
+    this.name = name;
+    this.balance = balance;
+    this.animalType = animalType;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public long getBalance() {
+    return balance;
+  }
+
+  public void setBalance(long balance) {
+    this.balance = balance;
+  }
+
+  public String getAnimalType() {
+    return animalType;
+  }
+
+  public void setAnimalType(String animalType) {
+    this.animalType = animalType;
+  }
+}
