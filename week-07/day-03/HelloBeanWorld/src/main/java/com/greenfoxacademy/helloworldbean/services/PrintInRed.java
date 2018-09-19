b8fed9814de4d92ffb96c410e4b692d9b5c68794
Laszlo Daru <daruu@hotmail.com>
2018-09-19 13:42:50 +0200

    Second Task Final

--- week-07/day-03/HelloBeanWorld/src/main/java/com/greenfoxacademy/helloworldbean/services/PrintInRed.java
+++ week-07/day-03/HelloBeanWorld/src/main/java/com/greenfoxacademy/helloworldbean/services/PrintInRed.java
@@ -1,11 +1,20 @@
 package com.greenfoxacademy.helloworldbean.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PrintInRed implements MyColor {
+
+  Printer redPrint;
+
+  @Autowired
+  public PrintInRed(Printer redPrint) {
+    this.redPrint = redPrint;
+  }
+
   @Override
   public void printColor() {
-    System.out.println("It's printing in Red!");
+    redPrint.log("It's printing in Red!");
   }
 }
--- week-07/day-03/HelloBeanWorld/src/main/java/com/greenfoxacademy/helloworldbean/services/PrintingInBlue.java
+++ week-07/day-03/HelloBeanWorld/src/main/java/com/greenfoxacademy/helloworldbean/services/PrintingInBlue.java
@@ -1,11 +1,18 @@
 package com.greenfoxacademy.helloworldbean.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PrintingInBlue implements MyColor {
+  Printer bueprint;
+@Autowired
+  public PrintingInBlue(Printer bueprint) {
+    this.bueprint = bueprint;
+  }
+
   @Override
   public void printColor() {
-    System.out.println("It's printing in Blue!");
+  bueprint.log("It's printing in Blure!");
   }
 }
