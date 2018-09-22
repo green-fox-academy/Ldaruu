dc701721f384c840284ec634103c92c784a25143
Laszlo Daru <daruu@hotmail.com>
2018-09-22 20:59:26 +0200

    Login page styled

--- week-07/day-04/FoxClub/src/main/java/com/greenfoxacademy/foxclub/MainController.java
+++ week-07/day-04/FoxClub/src/main/java/com/greenfoxacademy/foxclub/MainController.java
@@ -1,8 +1,16 @@
 package com.greenfoxacademy.foxclub;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
+
+  @GetMapping("/login")
+  public String login(){
+    return "login";
+  }
+
+
 }
