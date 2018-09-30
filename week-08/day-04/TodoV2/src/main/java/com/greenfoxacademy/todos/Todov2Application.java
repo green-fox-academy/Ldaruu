ff5be59e3952e44a159d7cfc68ce24e983982e5b
Laszlo Daru <daruu@hotmail.com>
2018-09-30 13:54:01 +0200

    chore(todoAppV2):initiate project

---
+++ week-08/day-04/TodoV2/src/main/java/com/greenfoxacademy/todos/Todov2Application.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.todos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Todov2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Todov2Application.class, args);
+  }
+}
