25b04c2fb48d61c3d1262001253f538513f371f9
Laszlo Daru <daruu@hotmail.com>
2018-10-13 00:45:38 +0200

    SecurityConfig added with basic authentication

---
+++ week-10/AuthenticationPracc/src/main/java/com/greenfoxacedemy/bank/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.greenfoxacedemy.bank.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableGlobalMethodSecurity(securedEnabled = true)
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+  @Autowired
+  public void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
+
+    auth
+        .inMemoryAuthentication()
+          .withUser("daruu")
+          .password("12345")
+          .roles("USER")
+        .and()
+          .withUser("admin")
+          .password("12345")
+          .roles("ADMIN");
+  }
+  protected void configure(HttpSecurity httpSec) throws Exception{
+
+    httpSec
+        .authorizeRequests()
+          .antMatchers(HttpMethod.GET,"/")
+          .permitAll()
+          .antMatchers("/users").hasAnyRole("ADMIN")
+        .and()
+          .formLogin()
+            .permitAll();
+
+  }
+}
