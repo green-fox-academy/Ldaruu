273209c5b7c6be182765c77b24d22fd20518d24b
Laszlo Daru <daruu@hotmail.com>
2018-10-14 21:19:53 +0200

    NoopPasswordEncoder added &still a mess

--- week-10/AuthenticationPracc/src/main/java/com/greenfoxacedemy/bank/config/SecurityConfig.java
+++ week-10/AuthenticationPracc/src/main/java/com/greenfoxacedemy/bank/config/SecurityConfig.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @Configuration
@@ -24,6 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Autowired
   private UserDetailsService userService;
 
+//  @Override
 //  public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
 //    authenticationManagerBuilder
 //        .userDetailsService(userService)
@@ -31,10 +35,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //  }
 //
 //  @Bean
-//  public BCryptPasswordEncoder passwordEncoder() {
+//  @Override
+//  public AuthenticationManager authenticationManagerBean() throws Exception {
+//    return super.authenticationManagerBean();
+//  }
+//
+//  @Bean
+//  public PasswordEncoder passwordEncoder() {
 //    return new BCryptPasswordEncoder();
 //  }
 
+  @SuppressWarnings("deprecation")
+  @Bean
+  public static NoOpPasswordEncoder passwordEncoder() {
+    return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+  }
 
   @Autowired
   public void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
