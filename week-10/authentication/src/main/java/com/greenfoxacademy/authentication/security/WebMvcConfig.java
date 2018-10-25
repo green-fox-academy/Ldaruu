3fb72be1042dea191112410ed214b78050286181
Laszlo Daru <daruu@hotmail.com>
2018-10-25 16:37:30 +0200

    app-propeties added

--- week-10/authentication/src/main/java/com/greenfoxacademy/authentication/security/WebMvcConfig.java
+++ week-10/authentication/src/main/java/com/greenfoxacademy/authentication/security/WebMvcConfig.java
@@ -1,6 +1,14 @@
 package com.greenfoxacademy.authentication.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class WebMvcConfig implements WebMvcConfigurer {
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+    return bCryptPasswordEncoder;
+  }
 }
